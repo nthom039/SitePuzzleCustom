3948492a235c6b471198a4b76e95f0e74c670920
https://git.ongarde.net/NoahT <nthom039@uottawa.ca>
2022-07-15 10:35:15 -0400

    chek=c

---
+++ src/TopBar.js
@@ -0,0 +1,10 @@
+function TopBar() {
+    return(
+        <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#news">News</a></li>
+        </ul>
+    );
+}
+
+export default TopBar;
